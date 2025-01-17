1a6b58e76a64776952ea54c3c3a0f3547d2e7fee
Sajo <sajo@ebi.ac.uk>
2025-01-17 18:17:16 +0000

    goci-1461-cleanup-homes-dir adding output logging for sbatch scripts

--- goci-tools/goci-deposition-sync/src/main/resources/scripts/run-slurm-sync-wrapper.sh
+++ goci-tools/goci-deposition-sync/src/main/resources/scripts/run-slurm-sync-wrapper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH -t 04:00:00
-
 #SBATCH --mem=4G
+#SBATCH --output=/hps/nobackup/parkinso/spot/gwas/logs/sbatch/slurm-%j.out
+#SBATCH --error=/hps/nobackup/parkinso/spot/gwas/logs/sbatch/slurm-%j.err
 mode=${1}
 echo "mode is ${mode}"
 if [ -z "$1" ]
